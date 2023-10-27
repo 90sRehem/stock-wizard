5cd69943285ff3048f4950baab3bb043c85daf7a
Jonathan Rehem <jonathan.rehem@outlook.com>
2023-10-26 21:38:20 -0300

    feat: app structure

---
+++ apps/web/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "ui";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-red-600">Stock-Wizard</h1>
+      <Button>click me</Button>
+    </>
+  );
+}
+
+export default App;
---
+++ packages/eslint-config-custom/react.js
@@ -0,0 +1,34 @@
+const { resolve } = require("node:path");
+
+const project = resolve(process.cwd(), "tsconfig.json");
+
+/*
+ * This is a custom ESLint configuration for use with
+ * Next.js apps.
+ *
+ * This config extends the Vercel Engineering Style Guide.
+ * For more information, see https://github.com/vercel/style-guide
+ *
+ */
+
+module.exports = {
+  extends: [
+    "@rehem-packages/eslint-config/react",
+    "eslint-config-turbo",
+  ].map(require.resolve),
+  parserOptions: {
+    project,
+  },
+  globals: {
+    React: true,
+    JSX: true,
+  },
+  settings: {
+    "import/resolver": {
+      typescript: {
+        project,
+      },
+    },
+  },
+  ignorePatterns: ["node_modules/", "dist/"],
+};
