215dfd9b8a5372d30a8def7e4fd52704222ecadb
Jonathan Rehem <jonathan.rehem@outlook.com>
2023-12-17 13:22:19 -0300

    feat: Update app.controller.ts with authentication and error handling

--- apps/gateway/src/app.controller.ts
+++ apps/gateway/src/app.controller.ts
@@ -6,10 +6,11 @@ import {
   HttpException,
   Inject,
   Post,
+  Req,
 } from '@nestjs/common';
 import { AppService } from './app.service';
 import { ClientProxy } from '@nestjs/microservices';
-import { firstValueFrom } from 'rxjs';
+import { firstValueFrom, lastValueFrom } from 'rxjs';
 
 type CreateUserDTO = {
   name: string;
@@ -22,7 +23,7 @@ export class AppController {
   constructor(
     private readonly appService: AppService,
     @Inject('USER_SERVICE') private readonly userClient: ClientProxy,
-    @Inject('AUTH_SERVICE') private readonly authService,
+    @Inject('AUTH_SERVICE') private readonly authService: ClientProxy,
   ) { }
 
   @Get()
@@ -31,8 +32,15 @@ export class AppController {
   }
 
   @Get('users')
-  async listUsers() {
-    const response = await this.userClient.send('listUsers', {}).toPromise();
+  async listUsers(@Req() req: any) {
+    const authorization = req.headers.authorization;
+
+    await firstValueFrom(
+      this.authService.send({ cmd: 'verify' }, { authorization }),
+    );
+
+    const pattern = { cmd: 'list-users' };
+    const response = await firstValueFrom(this.userClient.send(pattern, {}));
     return response;
   }
 
@@ -46,7 +54,7 @@ export class AppController {
       this.userClient.send(pattern, payload),
     );
 
-    if (response.error) {
+    if (response?.error) {
       throw new HttpException(
         {
           message: response.message,
@@ -63,6 +71,22 @@ export class AppController {
   async authenticate(
     @Body() { email, password }: { email: string; password: string },
   ) {
-    return await this.authService.send('authenticate', { email, password });
+    const pattern = { cmd: 'authenticate' };
+    const payload = { email, password };
+    const response = await lastValueFrom(
+      this.authService.send(pattern, payload),
+    );
+
+    if (response.error) {
+      throw new HttpException(
+        {
+          message: response.error.message,
+          errors: response.error.errors,
+        },
+        response.error.statusCode,
+      );
+    }
+
+    return response;
   }
 }
