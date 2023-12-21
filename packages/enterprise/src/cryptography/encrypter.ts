ea938c9e2feb14e86e4b15630d912cc411c550ea
Jonathan Rehem <jonathan.rehem@outlook.com>
2023-12-20 21:07:02 -0300

    Add SignOptions to encrypt method

--- packages/enterprise/src/cryptography/encrypter.ts
+++ packages/enterprise/src/cryptography/encrypter.ts
@@ -1,4 +1,6 @@
+import type { SignOptions } from 'jsonwebtoken';
+
 export abstract class Encrypter {
-  abstract encrypt(payload: Record<string, unknown>): Promise<string>;
+  abstract encrypt(payload: Record<string, unknown>, options?: SignOptions): Promise<string>;
   abstract decrypt(token: string): Promise<Record<string, unknown>>;
 }
